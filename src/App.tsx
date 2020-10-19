450808dc41fad5bf7f759d3b5ede84c3e463a46e
신윤호 <amuse1991@gmail.com>
2020-10-19 18:50:19 +0900

    타입 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import MatrixContainer from "./containers/MatrixContainer";
 import "./styles.css";
-import Matrix from "./Matrix";
 
 export default function App() {
   return (
     <div className="App">
       <h1>JS Matrix</h1>
       <MatrixContainer />
-      <Matrix />
     </div>
   );
 }
